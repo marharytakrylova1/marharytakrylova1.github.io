53312e462f8afe3c8ffb6c66bb32d95a2326b2c7
Luke Sutor <lukesutor@gmail.com>
2023-12-25 14:05:42 -0500

    change site to use gatsby

--- src/pages/Involvement.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function Involvement() {
-    return(
-        <div>involvement</div>
-    );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    `./src/pages/**/*.{js,jsx,ts,tsx}`,
+    `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {
-      colors: {
-        duke: "#00539b",
-      },
-      pastel: {
-        blue: "#C6DEF1",
-        pink: "#F2C6DE",
-        green: "#C9E4DE"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
-
